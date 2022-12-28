0698bbb9f058258bb5409c1a38062ce89c9ddd34
PankajDeshmukh1999 <pankajdeshmukh1999@gmail.com>
2022-12-28 13:20:32 +0530

    tesing

--- fund-transfer/src/main/java/com/fund/transfer/entity/RemitterRegistration.java
+++ fund-transfer/src/main/java/com/fund/transfer/entity/RemitterRegistration.java
@@ -5,8 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+
 
 @Table(name = "registration")
 public class RemitterRegistration {
--- fund-transfer/src/test/java/com/fund/transfer/FundTransferApplicationTests.java
+++ fund-transfer/src/test/java/com/fund/transfer/FundTransferApplicationTests.java
@@ -1,7 +1,18 @@
 package com.fund.transfer;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fund.transfer.entity.Transaction;
+import com.fund.transfer.repository.TransactionRepository;
 
 @SpringBootTest
 class FundTransferApplicationTests {
@@ -9,5 +20,17 @@ class FundTransferApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Autowired
+	private TransactionRepository repository;
+	
+	private Transaction transaction;
+	
+	@Test
+	public void getAllTransactionTest() {
+		List l = repository.findAll();
+		assertThat(l).size().isGreaterThan(0);
+	}
+	
 
 }
